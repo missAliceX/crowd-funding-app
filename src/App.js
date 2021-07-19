c46bf45850d30c7cffd055574d7adc860698da69
Alice Xia <67359807+missAliceX@users.noreply.github.com>
2021-07-18 21:38:40 -0700

    add router (#8)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProjectProposalPage from "pages/project-proposal";
 import "index.css";
 import "styles/text.scss";
 
 function App() {
-  return <ProjectProposalPage />;
+  // path="/" is the URL when the app loads, we are choosing to show the ProjectProposalPage component
+  return (
+    <Router>
+      <Route path="/" component={ProjectProposalPage} />
+      <Route path="/project/proposal" component={ProjectProposalPage} />
+    </Router>
+  );
 }
 
 export default App;
