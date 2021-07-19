e58f2a359c1c40ad5ff46d351b05f3a6617ae01a
Alice Xia <67359807+missAliceX@users.noreply.github.com>
2021-07-18 21:20:34 -0700

    update navbar test (#7)
    * rename to navbar, responsiv-ize it
    
    * Update navbar.test.js

--- src/components/navbar.test.js
+++ src/components/navbar.test.js
@@ -2,10 +2,25 @@ import common from "styles/common.module.scss";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Navbar from "components/navbar";
 
-test("render navbar", () => {
+const resizeWindow = (width, height) => {
+  window.innerWidth = width;
+  window.innerHeight = height;
+  window.dispatchEvent(new Event("resize"));
+};
+
+test("render navbar landscape", () => {
   render(<Navbar data-testid="navbar" />);
 
   const navbar = screen.getByTestId("navbar");
   expect(navbar).toBeInTheDocument();
   expect(navbar).toHaveStyle(`width: ${common.navbarwidth}`);
+});
+
+test("render navbar portrait", () => {
+  render(<Navbar data-testid="navbar" />);
+  resizeWindow(250, 600);
+
+  const navbar = screen.getByTestId("navbar");
+  expect(navbar).toBeInTheDocument();
+  expect(navbar).toHaveStyle(`width: 250`);
 });
